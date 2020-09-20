37e02f508d33c6ab3e0769970c40ffea0e78b19e
Thijs Daniels <info@thijsdaniels.com>
2020-09-20 16:49:19 +0200

    fix(routes): resolve merge conflict

--- src/router/index.js
+++ src/router/index.js
@@ -5,23 +5,6 @@ import { Home, Settings, Login } from "../views";
 Vue.use(VueRouter);
 
 const routes = [
-<<<<<<< HEAD
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/settings",
-    name: "Settings",
-    component: Settings,
-  },
-  {
-    path: "/login",
-    name: "Login",
-    component: Login,
-  },
-=======
 	{
 		path: "/",
 		name: "Home",
@@ -32,7 +15,11 @@ const routes = [
 		name: "Settings",
 		component: Settings,
 	},
->>>>>>> 481bc4103331842c02eb32089a1316a165f245d7
+	{
+		path: "/login",
+		name: "Login",
+		component: Login,
+	},
 ];
 
 const router = new VueRouter({
