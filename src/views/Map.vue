65131e87733e2b7cb3f17af7e65cf63a9eff8588
brambekkers <bram.bekkers@outlook.com>
2020-10-03 19:31:23 +0200

    Map pitch en zoom

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,19 +1,18 @@
 <template>
-    <img id="map" ref="map" src="@/assets/skimap.jpg" />
+    <pinch-zoom id="map">
+        <img ref="map" src="@/assets/skimap.jpg" />
+    </pinch-zoom>
 </template>
 
 <script>
-import PinchZoom from "pinch-zoom-js";
+import PinchZoom from "vue-pinch-zoom";
 export default {
-    mounted() {
-        new PinchZoom(this.$refs.map, {
-            maxZoom: 7
-        });
-    }
+    components: { PinchZoom }
 };
 </script>
 
 <style lang="scss" scoped>
 #map {
+    height: calc(100vh - 56px);
 }
 </style>
