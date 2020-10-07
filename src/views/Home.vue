af73bd873f2328b2dd6b395b6cf4a2c1d4b01c86
brambekkers <bram.bekkers@outlook.com>
2020-10-07 09:53:13 +0200

    add image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,21 @@
 <template>
 	<v-container>
-		<v-row class="justify-center">
-			<v-col
-				cols="8"
-				class="pt-0 mt-n4"
-			>
+		<v-row class="justify-center" align="center">
+			<v-col cols="8" md="4" xl="2" class="pt-0 mt-n4">
 				<v-img
 					contain
 					src="@/assets/happySkiFriends/skilift.png"
 				></v-img>
 			</v-col>
 			<Countdown />
-
 		</v-row>
 	</v-container>
 </template>
 
 <script>
-import Countdown from "@/components/Countdown.vue";
-export default {
-	name: "Home",
-	components: { Countdown },
-};
+	import Countdown from "@/components/Countdown.vue";
+	export default {
+		name: "Home",
+		components: { Countdown },
+	};
 </script>
