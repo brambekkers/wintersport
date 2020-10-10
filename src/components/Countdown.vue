9b151b584a729335d38fcdbad2ccacd6ac1945d3
brambekkers <bram.bekkers@outlook.com>
2020-10-10 19:21:49 +0200

    fix text position countdown

--- src/components/Countdown.vue
+++ src/components/Countdown.vue
@@ -1,103 +1,101 @@
 <template>
-	<v-col cols="7" sm="5" md="4" lg="3" xl="2">
-		<v-card elevation="4">
-			<div id="countdown" class="pa-0">
-				<v-card class="number" light>
-					<span class="hanger hanger-left"></span>
-					<span class="hanger hanger-right"></span>
-					<span class="topBar"></span>
-					<p>{{ daysToGo }}</p>
-				</v-card>
-				<v-card class="daysToGo text-h4">
-					<p>Days to go</p>
-				</v-card>
-			</div>
-		</v-card>
-	</v-col>
+    <v-col cols="7" sm="5" md="4" lg="3" xl="2">
+        <v-card elevation="4">
+            <div id="countdown" class="pa-0">
+                <v-card class="number" light>
+                    <span class="hanger hanger-left"></span>
+                    <span class="hanger hanger-right"></span>
+                    <span class="topBar"></span>
+                    <p>{{ daysToGo }}</p>
+                </v-card>
+                <v-card class="daysToGo text-h4">
+                    <p>Days to go</p>
+                </v-card>
+            </div>
+        </v-card>
+    </v-col>
 </template>
 
 <script>
-	export default {
-		computed: {
-			daysToGo() {
-				const countDownDay = [20, 2]; // 20th of February
-				const today = new Date();
-				const cday = new Date(
-					today.getFullYear(),
-					countDownDay[1] - 1,
-					countDownDay[0]
-				);
-				if (today.getTime() > cday.getTime()) {
-					cday.setFullYear(cday.getFullYear() + 1);
-				}
-				const diff = cday.getTime() - today.getTime();
+export default {
+    computed: {
+        daysToGo() {
+            const countDownDay = [20, 2]; // 20th of February
+            const today = new Date();
+            const cday = new Date(today.getFullYear(), countDownDay[1] - 1, countDownDay[0]);
+            if (today.getTime() > cday.getTime()) {
+                cday.setFullYear(cday.getFullYear() + 1);
+            }
+            const diff = cday.getTime() - today.getTime();
 
-				return Math.floor(diff / (1000 * 60 * 60 * 24));
-			},
-		},
-	};
+            return Math.floor(diff / (1000 * 60 * 60 * 24));
+        }
+    }
+};
 </script>
 
 <style lang="scss" scoped>
-	#countdown {
-		display: flex;
-		text-align: center;
+#countdown {
+    display: flex;
+    text-align: center;
 
-		div {
-			font-size: 2rem;
-			font-weight: 600;
-			display: flex;
-			justify-content: center;
-			align-items: center;
+    div {
+        font-size: 2rem;
+        font-weight: 600;
+        display: flex;
+        justify-content: center;
+        align-items: center;
 
-			p {
-				margin: 0;
-			}
-		}
+        p {
+            margin: 0;
+        }
+    }
 
-		.number {
-			position: relative;
-			flex: 3;
-			flex-direction: column;
-			min-height: 90px;
-			justify-content: flex-start;
+    .number {
+        position: relative;
+        flex: 3;
+        flex-direction: column;
+        min-height: 90px;
+        justify-content: flex-start;
 
-			p {
-				padding: 1rem;
-				min-height: 50% !important;
-			}
+        p {
+            padding: 1rem;
+            min-height: 50% !important;
+            display: flex;
+            align-items: center;
+        }
 
-			.topBar {
-				min-height: 30% !important;
-				width: 100%;
-				background: #ed1b24;
-			}
+        .topBar {
+            min-height: 30% !important;
+            width: 100%;
+            background: #ed1b24;
+        }
 
-			.hanger {
-				position: absolute;
-				top: -8%;
-				height: 20%;
-				width: 6%;
-				border-radius: 25%;
-				z-index: 2;
-				background: #202028 !important;
-			}
+        .hanger {
+            position: absolute;
+            top: -8%;
+            height: 20%;
+            width: 6%;
+            border-radius: 25%;
+            z-index: 2;
+            background: #202028 !important;
+        }
 
-			.hanger-left {
-				left: 20%;
-			}
-			.hanger-right {
-				right: 20%;
-			}
-		}
+        .hanger-left {
+            left: 20%;
+        }
+        .hanger-right {
+            right: 20%;
+        }
+    }
 
-		.daysToGo {
-			padding: 1rem;
-			flex: 4;
-			background: #202028 !important;
-			color: white;
-			line-height: 1.8rem;
-			border-radius: 0 4px 4px 0;
-		}
-	}
+    .daysToGo {
+        padding: 1rem;
+        flex: 4;
+        background: #202028 !important;
+        color: white;
+        line-height: 1.8rem;
+        border-radius: 0 4px 4px 0;
+    }
+}
 </style>
