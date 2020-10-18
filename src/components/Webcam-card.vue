503f93d64219d223e60b508190f4ecffc7ea9eb3
Bram Bekkers <bram.bekkers@outlook.com>
2020-10-18 21:05:24 +0200

    Update webcam card home

--- src/components/Webcam-card.vue
+++ src/components/Webcam-card.vue
@@ -1,26 +1,41 @@
 <template>
-<v-col cols="6" sm="3" lg="3" xl="2" >
-	<v-card class="h-100 d-flex flex-column">
-		<v-card-title class="text-h6"> Webcams </v-card-title>
-		<v-card-subtitle>{{ onlineAmount }} online</v-card-subtitle>
-		<v-card-actions class="align-self-end mt-auto mb-3 mr-3">
-			<v-btn rounded outlined> Watch now </v-btn>
-		</v-card-actions>
-	</v-card>
-</v-col>
+	<v-col
+		cols="6"
+		sm="3"
+		lg="3"
+		xl="2"
+	>
+		<v-card class="h-100 d-flex flex-column">
+			<v-card-title class="text-h6"> Webcams </v-card-title>
+			<v-card-subtitle>
+				<v-icon
+					color="primary"
+					small
+				>play_circle_filled</v-icon>
+				{{ onlineAmount }} online
+			</v-card-subtitle>
+			<v-card-actions class="align-self-end mt-auto mb-3 mr-3">
+				<v-btn
+					rounded
+					outlined
+					to="webcams"
+				> Watch now </v-btn>
+			</v-card-actions>
+		</v-card>
+	</v-col>
 </template>
 
 <script>
-	import { mapGetters } from "vuex";
+import { mapGetters } from "vuex";
 
-	export default {
-		computed: {
-			...mapGetters(["allCams"]),
-			onlineAmount() {
-				return this.allCams.filter((c) => c.online).length;
-			},
+export default {
+	computed: {
+		...mapGetters(["allCams"]),
+		onlineAmount() {
+			return this.allCams.filter((c) => c.online).length;
 		},
-	};
+	},
+};
 </script>
 
 <style>
