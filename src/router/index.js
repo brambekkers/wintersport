113592f24b23670e501b8c9e09683147069d2d4e
Bram Bekkers <bram.bekkers@outlook.com>
2020-10-18 11:51:18 +0200

    route fix

--- src/router/index.js
+++ src/router/index.js
@@ -23,153 +23,153 @@ import AddUser from "../views/users/Add";
 Vue.use(VueRouter);
 
 const routes = [
-    {
-        path: "/",
-        name: "Happy Ski Friends",
-        component: Home,
-        meta: {
-            requiresAuth: true
-          }
-        // beforeEnter: (to, from, next) => {
-        //     if (!firebase.apps.length) {
-        //         setTimeout(() => {
-        //             if (firebase.auth().currentUser) {
-        //                 next();
-        //             } else {
-        //                 next("/login");
-        //             }
-        //         }, 1000);
-        //     } else {
-        //         if (firebase.auth().currentUser) {
-        //             next();
-        //         } else {
-        //             next("/login");
-        //         }
-        //     }
-        // }
-    },
-    {
-        path: "/settings",
-        name: "Settings",
-        component: Settings,
-        meta: {
-            requiresAuth: true
-          }
-    },
-    {
-        path: "/login",
-        name: "Login",
-        component: Login,
-        meta: {
-            requiresAuth: true
-          }
-    },
-    {
-        path: "/weather",
-        name: "Weather",
-        component: Weather,
-        meta: {
-            requiresAuth: true
-          }
-    },
-    {
-        path: "/map",
-        name: "Map",
-        component: Maps,
-        meta: {
-            requiresAuth: true
-          }
-    },
-    {
-        path: "/location",
-        name: "Location",
-        component: Location,
-        meta: {
-            requiresAuth: true
-          }
-    },
-    {
-        path: "/routes",
-        name: "Routes",
-        component: Routes,
-        meta: {
-            requiresAuth: true
-          }
-    },
-    {
-        path: "/webcams",
-        name: "Webcams",
-        component: Webcams,
-        meta: {
-            requiresAuth: true
-          }
-    },
-    {
-        path: "/photos",
-        name: "Photos",
-        component: Photos,
-        meta: {
-            requiresAuth: true
-          }
-    },
-    {
-        path: "/chat",
-        name: "Chat",
-        component: Chat,
-        meta: {
-            requiresAuth: true
-          }
-    },
-
-    {
-        path: "/changepassword",
-        name: "Change Password",
-        component: ChangePassword,
-        meta: {
-            requiresAuth: true
-          }   
-    },
-    {
-        path: "/forgotpassword",
-        name: "Forgot Password",
-        component: ForgotPassword,
-        meta: {
-            requiresAuth: true
-          }   
-    },
-    // Users
-    {
-        path: "/users",
-        name: "Users",
-        component: Users,
-        meta: {
-            requiresAuth: true
-          }
-    },
-    {
-        path: "/users/add",
-        name: "Add user",
-        component: AddUser,
-        meta: {
-            requiresAuth: true
-          }
+  {
+    path: "/",
+    name: "Happy Ski Friends",
+    component: Home,
+    meta: {
+      requiresAuth: true
     }
+    // beforeEnter: (to, from, next) => {
+    //     if (!firebase.apps.length) {
+    //         setTimeout(() => {
+    //             if (firebase.auth().currentUser) {
+    //                 next();
+    //             } else {
+    //                 next("/login");
+    //             }
+    //         }, 1000);
+    //     } else {
+    //         if (firebase.auth().currentUser) {
+    //             next();
+    //         } else {
+    //             next("/login");
+    //         }
+    //     }
+    // }
+  },
+  {
+    path: "/settings",
+    name: "Settings",
+    component: Settings,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+    meta: {
+      requiresAuth: false
+    }
+  },
+  {
+    path: "/weather",
+    name: "Weather",
+    component: Weather,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: "/map",
+    name: "Map",
+    component: Maps,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: "/location",
+    name: "Location",
+    component: Location,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: "/routes",
+    name: "Routes",
+    component: Routes,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: "/webcams",
+    name: "Webcams",
+    component: Webcams,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: "/photos",
+    name: "Photos",
+    component: Photos,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: "/chat",
+    name: "Chat",
+    component: Chat,
+    meta: {
+      requiresAuth: true
+    }
+  },
+
+  {
+    path: "/changepassword",
+    name: "Change Password",
+    component: ChangePassword,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: "/forgotpassword",
+    name: "Forgot Password",
+    component: ForgotPassword,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  // Users
+  {
+    path: "/users",
+    name: "Users",
+    component: Users,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: "/users/add",
+    name: "Add user",
+    component: AddUser,
+    meta: {
+      requiresAuth: true
+    }
+  }
 ];
 
 const router = new VueRouter({
-    mode: "history",
-    routes
+  mode: "history",
+  routes
 });
 
 router.beforeEach(async (to, from, next) => {
-    const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
 
-    // Check if your logged in
-    if (requiresAuth && !await store.dispatch('getCurrentUser')){
-      next('login');
-    }else{
-      next();
-    }
-  });
+  // Check if your logged in
+  if (requiresAuth && !await store.dispatch('getCurrentUser')) {
+    next('login');
+  } else {
+    next();
+  }
+});
 
 export default router;
